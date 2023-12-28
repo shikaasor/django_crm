6ead61500df1a9b8e93ed1f4fe425b4b79f22e15
kaasor <asor.tar23@gmail.com>
2023-12-28 06:02:09 +0100

    update record added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -70,5 +70,27 @@ def delete_record(request, pk):
         return redirect('home')
 
 def add_record(request):
+    form = AddRecordForm(request.POST or None)
     if request.user.is_authenticated:
-        return render(request, 'add_record.html', {})
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Record added!')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.error(request, 'Authentication required')
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Record updated!')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.error(request, 'Authentication required')
+        return redirect('home')
